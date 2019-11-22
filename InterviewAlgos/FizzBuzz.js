a88c2ba971897140bf00def7898ba2346ebc9bd5
Cody Baermann <cbaermann@comcast.net>
2019-11-22 05:50:39 -0800

    alternate chunked solution

--- InterviewAlgos/FizzBuzz.js
+++ InterviewAlgos/FizzBuzz.js
@@ -23,3 +23,9 @@ function fizzBuzz(n)
         }
     }
 }
+//const chunked = []
+//let index = 0;
+//while(index<array.length){
+    //chunked.push(array.slice(index, index+size))
+    //index += size;
+//}
