baa21cc903998c3242f6fa79d347a81693dc8253
Cody Baermann <cbaermann@comcast.net>
2019-11-23 23:31:20 -0800

    revised capitalize

--- InterviewAlgos/capitalize.js
+++ InterviewAlgos/capitalize.js
@@ -1,7 +1,10 @@
 function capitalize(str) {
+    //empty array
     const words = [];
+    //eliminates spaces
     for(let word of str.split(' '))
     {
+        //capitalizes first word of sentence
         words.push(word[0].toUpperCase() + word.slice(1))
     }
     return words.join(' ')
